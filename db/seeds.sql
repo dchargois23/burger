7e6dd812428350861b4eecd97c7192e449b85ca4
Derrick Chargois <36555200+dchargois23@users.noreply.github.com>
2020-06-14 15:00:47 -0500

    add connections

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Cheese Burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Double Cheese Burger');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Fish Burger', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Chicken', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Impossible Burger', true);
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Veggie');
